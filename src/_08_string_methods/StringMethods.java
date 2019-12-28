9f3d69a0f98d555275df835e091ef8c1b17d252e
League Student <league@iMac-18.attlocal.net>
2019-12-27 16:57:54 -0800

    Happy Pet

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -7,12 +7,11 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-String s = "cats";
+String s = "dogs";
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		// System.out.println(s.charAt(3));
+		 //System.out.println(s.charAt(3));
 		// 3. Print the length of your String to the console.
-		//    HINT: .length()
+		//System.out.println(s.length());
         // System.out.println(s.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
@@ -25,7 +24,7 @@ String s = "cats";
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		System.out.println(s.charAt(1));
 	}
 }
 
--- src/_09_pi_aloud/PiAloud.java
+++ src/_09_pi_aloud/PiAloud.java
@@ -6,26 +6,31 @@ package _09_pi_aloud;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class PiAloud {
 
+
 	// 1. Make a main method and make sure your program can run
-
+public static void main(String[] args) {
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String s = "http://www.piday.org/million/";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+ s.charAt(1);
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+for (int i = 0; i < s.length(); i++) {
+	System.out.println(i);
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
-
+speak(s);
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
-	static void speak(char characterToSpeak) {
+}
+	static void speak(String s) {
 		try {
-			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
+			Runtime.getRuntime().exec("say " + s).waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
